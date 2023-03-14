984835ad67559b37634455ca6f9589c53a3fb883
Fahim Kamal Ahmed <fahimkamal63@gmail.com>
2023-03-14 15:17:22 +0600

    new event system added.

---
+++ Popup test/Assets/Event System SO/Events Scripts/IntEvent.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Events_Scripts
+{
+    [CreateAssetMenu(menuName = "Events/int Event")]
+    public class IntEvent : BaseEvent<int>
+    {
+    
+    }
+}
---
+++ Popup test/Assets/Event System SO/Events Scripts/StringEvent.cs
@@ -0,0 +1,9 @@
+using Events_Scripts;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(menuName = "Events/String Event")]
+public class StringEvent : BaseEvent<string>
+{
+    
+}

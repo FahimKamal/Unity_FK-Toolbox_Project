14db3cb7e95eefc6bc509449502098a5871dfe83
Fahim Kamal Ahmed <fahimkamal63@gmail.com>
2023-05-21 15:35:48 +0600

    New Attribute: ShowOnlyFK created and added.
    This attribute will let you make a serialized filed read only in inspector. but you will be able to change them through code.

---
+++ FK-Toolbox/Assets/FK-Toolbox/Custom Attribute/Editor/ShowOnlyFKDrawer.cs
@@ -0,0 +1,22 @@
+using Custom_Attribute;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ShowOnlyFKAttribute))]
+public class ShowOnlyFKDrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property,
+        GUIContent label)
+    {
+        return EditorGUI.GetPropertyHeight(property, label, true);
+    }
+ 
+    public override void OnGUI(Rect position,
+        SerializedProperty property,
+        GUIContent label)
+    {
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = true;
+    }
+}
---
+++ FK-Toolbox/Assets/FK-Toolbox/Custom Attribute/ShowOnlyFKAttribute.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Custom_Attribute
+{
+    public class ShowOnlyFKAttribute : PropertyAttribute
+    {
+ 
+    }
+}

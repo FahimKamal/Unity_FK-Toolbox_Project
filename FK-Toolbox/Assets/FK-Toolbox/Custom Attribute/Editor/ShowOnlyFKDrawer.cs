459ae530fdbaafa609fc233b1526432e45648b16
Fahim Kamal Ahmed <fahimkamal63@gmail.com>
2023-06-11 17:48:00 +0600

    folder structure modified

--- FK-Toolbox/Assets/FK-Toolbox/Custom Attribute/Editor/ShowOnlyFKDrawer.cs
+++ FK-Toolbox/Assets/FK-Toolbox/Custom Attribute/Editor/ShowOnlyFKDrawer.cs
@@ -1,22 +1,24 @@
-using Custom_Attribute;
 using UnityEditor;
 using UnityEngine;
 
-[CustomPropertyDrawer(typeof(ShowOnlyFKAttribute))]
-public class ShowOnlyFKDrawer : PropertyDrawer
+namespace Custom_Attribute
 {
-    public override float GetPropertyHeight(SerializedProperty property,
-        GUIContent label)
+    [CustomPropertyDrawer(typeof(ShowOnlyFKAttribute))]
+    public class ShowOnlyFKDrawer : PropertyDrawer
     {
-        return EditorGUI.GetPropertyHeight(property, label, true);
-    }
- 
-    public override void OnGUI(Rect position,
-        SerializedProperty property,
-        GUIContent label)
-    {
-        GUI.enabled = false;
-        EditorGUI.PropertyField(position, property, label, true);
-        GUI.enabled = true;
+        public override float GetPropertyHeight(SerializedProperty property,
+            GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+
+        public override void OnGUI(Rect position,
+            SerializedProperty property,
+            GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = true;
+        }
     }
 }
